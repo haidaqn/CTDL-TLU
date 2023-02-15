87e6453e9cd9d0b833bbd9c5bf66304b8df71b43
Dang <dang14893@gmail.com>
2023-02-15 11:03:58 +0700

    sort

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -27,8 +27,8 @@ void SelectionSort(int n, int arr[])
 
 //chen
 void InsertionSort(int n, int arr[]) {
-    int i, key, j;
-    for (i = 1; i < n; i++)
+    int key, j;
+    for (int i = 1; i < n; i++)
     {
         key = arr[i];
         j = i - 1;
@@ -41,8 +41,52 @@ void InsertionSort(int n, int arr[]) {
     }
 }
 
-// nhanh tron 
+// tron 
 
+void Merge(int a[], int s, int f)
+{
+  int m = (s+f)/2;
+  int *b = new int[f-s+1];
+  int l = s, r = m+1;
+  int ib = 0;
+  while (l <=m && r <= f)
+  {
+    if (a[l-1] <= a[r-1])
+    {
+      b[ib++] = a[l-1];
+      l++;
+    }
+    else
+    {
+      b[ib++] = a[r-1];
+      r++;
+    }
+  }
+  if (l <= m)
+  {
+    for(int i = l; i <= m; i++)      
+      b[ib++] = a[i-1];
+  }
+  else if (r <= f)
+  {
+    for(int i = r; i<= f; i++)
+      b[ib++] = a[i-1];
+  }
+  for(int i = 0; i< ib; i++)
+  {
+    a[s-1+i] = b[i];
+  }
+}
+void MergeSort(int a[], int s, int f)
+{
+  if (s < f)
+  {
+    int m = (s + f) / 2;
+    MergeSort(a, s, m);
+    MergeSort(a, m+1, f);
+    Merge(a, s, f);
+  }
+}
 
 int main()
 {
@@ -51,6 +95,9 @@ int main()
 
     // SelectionSort(n, arr);
     // InsertionSort(n, arr);
+
+    MergeSort(arr,1,5);
+
     PrintAll(n, arr);
 
     return 0;
